3371776e7edcc54d3f2c70acb9e78c201308b58c
aleksandra_pereval <perevalova17_06@mail.ru>
2022-08-02 18:13:34 +0900

    вспоминаю курс

---
+++ example/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] array = new int[12];
+
+SetArray(array);
+
+array = SetArrayFunc(array.Length);
+
+void SetArray(int[] currentArray)
+{
+    Random rand = new Random();
+    for (int i=0; i < currentArray.Length; i++)
+    {
+        currentArray[i] = rand.Next(-9, 10);
+    }
+}
+
+int[] SetArrayFunc(int size)
+{
+    int[] newArray = new int[size];
+    Random rand = new Random();
+    for (int i=0; i < newArray.Length; i++);
+    {
+        newArray[] = rand.Next(-9, 10);
+    }
+    return newArray;
+}
